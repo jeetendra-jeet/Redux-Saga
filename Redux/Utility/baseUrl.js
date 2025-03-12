020d4d90557e0da3be71d09038ef723a489f8135
Jeetendra Kumar <67962129+jeetendra-jeet@users.noreply.github.com>
2025-03-12 11:21:39 +0530

    Update baseUrl.js

--- Redux/Utility/baseUrl.js
+++ Redux/Utility/baseUrl.js
@@ -1,5 +1,2 @@
 /* eslint-disable prettier/prettier */
-export const baseUrl = 'https://www.abninspire.com/api/';
-//export const baseUrl = 'https://chetu-staging.abninspirestaging.com/api/';
-// export const baseUrl = 'https://www.stage.abninspire.com/api/';
-export const privacyPolicy = 'https://www.abninspire.com/privacy-policy';
+export const baseUrl = 'anyEndPoints';
